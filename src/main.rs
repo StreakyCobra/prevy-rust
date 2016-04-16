a50c34bab6381ebd5380958ed551ca0bf4b81c46
Fabien Dubosson <fabien.dubosson@gmail.com>
2016-04-16 16:57:52 +0200

    Introduce clap for arguments parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+extern crate clap;
+
+use clap::{ArgMatches, App};
+
+fn parse_arguments<'a>() -> ArgMatches<'a> {
+    let matches = App::new("prevy")
+        .version("0.1.0")
+        .author("Fabien Dubosson <fabien.dubosson@gmail.com>")
+        .about("Manage your development workspaces with ease.")
+        .get_matches();
+
+    // Return the arguments
+    matches
+}
+
 fn main() {
-    println!("Hello, world!");
+    let _ = parse_arguments();
 }
