873e1fb7b092a80e1d956c4eeaf98d86aa53465d
Fabien Dubosson <fabien.dubosson@gmail.com>
2016-04-17 21:21:37 +0200

    Refactor display stdout/stderr in their own method

--- src/display.rs
+++ src/display.rs
@@ -1,12 +1,22 @@
 use std::io::{self, Write};
 use ansi_term::Colour::{Red, Blue};
 
+/// Print a given text to the standard output.
+fn stdout(text: &str) {
+    writeln!(io::stdout(), "{}", text).expect("Error writing Error to stdout");
+}
+
+/// Print a given text to the standard error.
+fn stderr(text: &str) {
+    writeln!(io::stderr(), "{}", text).expect("Error writing Error to stderr");
+}
+
 /// Print an error message.
 pub fn error(text: &str) {
-    writeln!(io::stderr(), "{}", Red.paint(text)).expect("Error writing Error to stderr");
+    stderr(&Red.paint(text).to_string());
 }
 
 /// Print an info message.
 pub fn info(text: &str) {
-    writeln!(io::stdout(), "{}", Blue.paint(text)).expect("Error writing Error to stdout");
+    stdout(&Blue.paint(text).to_string());
 }
