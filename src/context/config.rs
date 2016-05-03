c69b68d75d7e23f8d96d5d07a8399644a1c5a0d1
Fabien Dubosson <fabien.dubosson@gmail.com>
2016-05-03 19:44:02 +0200

    Improve config.rs documentation

--- src/context/config.rs
+++ src/context/config.rs
@@ -25,7 +25,7 @@ pub struct Config {
     pub nocolor: bool,
 }
 
-/// Define the default values for the config.
+/// Define the default values for the configurations.
 impl Default for Config {
     fn default() -> Config {
         Config {
@@ -39,6 +39,7 @@ impl Default for Config {
 // Macros                                                                    //
 // ------------------------------------------------------------------------- //
 
+/// Eval an command line argument as a boolean and store it in the context.
 macro_rules! eval_arg_bool {
     ($ctx:expr, $elem:expr, $id:expr) => (
         match $ctx.args.is_present($id) {
@@ -48,6 +49,7 @@ macro_rules! eval_arg_bool {
     )
 }
 
+/// Eval an environment variable as a boolean and store it in the context.
 macro_rules! eval_env_bool {
     ($ctx:expr, $elem:expr, $id:expr) => (
         match $ctx.env_vars.get($id) {
@@ -62,6 +64,7 @@ macro_rules! eval_env_bool {
     )
 }
 
+/// Eval a yaml as a boolean and store it in the context.
 macro_rules! eval_yaml_bool {
     ($yaml:expr, $elem:expr, $id:expr) => (
         match $yaml[$id].as_bool() {
@@ -75,7 +78,7 @@ macro_rules! eval_yaml_bool {
 // Public API                                                                //
 // ------------------------------------------------------------------------- //
 
-/// Get the context from default values and users preferences.
+/// Parse the configuration from default values and users preferences.
 ///
 /// The order of priority is the following:
 ///
@@ -85,40 +88,45 @@ macro_rules! eval_yaml_bool {
 /// 4. User configuration file
 pub fn parse_config(ctx: &mut Context) {
     // 4. User configuration file
-    read_config_file(ctx);
+    parse_config_file(ctx);
     // 3. Workspace file
-    read_workspace_file(ctx);
+    parse_workspace_file(ctx);
     // 2. Environment variables
-    read_env(ctx);
+    parse_env(ctx);
     // 1. Command line parameters
-    read_args(ctx);
+    parse_args(ctx);
 }
 
 // ------------------------------------------------------------------------- //
 // Internal functions                                                        //
 // ------------------------------------------------------------------------- //
 
-fn read_config_file(ctx: &mut Context) {
+/// Parse the configuration file.
+fn parse_config_file(ctx: &mut Context) {
     let yaml = ctx.configuration_file_content.clone();
-    read_yaml_config(ctx, yaml);
+    parse_yaml_config(ctx, yaml);
 }
 
-fn read_workspace_file(ctx: &mut Context) {
+/// Parse the workspace file.
+fn parse_workspace_file(ctx: &mut Context) {
     let yaml = ctx.workspace_file_content["config"].clone();
-    read_yaml_config(ctx, yaml);
+    parse_yaml_config(ctx, yaml);
 }
 
-fn read_args(ctx: &mut Context) {
+/// Parse command line arguments.
+fn parse_args(ctx: &mut Context) {
     eval_arg_bool!(ctx, ctx.config.debug, ID_CONFIG_DEBUG);
     eval_arg_bool!(ctx, ctx.config.nocolor, ID_CONFIG_NOCOLOR);
 }
 
-fn read_env(ctx: &mut Context) {
+/// Parse environment variables.
+fn parse_env(ctx: &mut Context) {
     eval_env_bool!(ctx, ctx.config.debug, ID_CONFIG_DEBUG);
     eval_env_bool!(ctx, ctx.config.nocolor, ID_CONFIG_NOCOLOR);
 }
 
-fn read_yaml_config(ctx: &mut Context, yaml: Yaml) {
+/// Parse a YAML configuration part.
+fn parse_yaml_config(ctx: &mut Context, yaml: Yaml) {
     eval_yaml_bool!(yaml, ctx.config.debug, ID_CONFIG_DEBUG);
     eval_yaml_bool!(yaml, ctx.config.nocolor, ID_CONFIG_NOCOLOR);
 }
